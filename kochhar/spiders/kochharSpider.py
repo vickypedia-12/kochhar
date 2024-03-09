e477e2812bca6dc9cb58f8469ecee726937c93f0
vickypedia-12 <vikasmourya54321@gmail.com>
2024-03-09 20:12:57 +0530

    content paged

--- kochhar/spiders/kochharSpider.py
+++ kochhar/spiders/kochharSpider.py
@@ -5,11 +5,11 @@ import pdfplumber
 import os
 import io
 
-
 class KochharSpider(scrapy.Spider):
     name = "kochhar"
+    page_number  = 2
     start_urls = [
-        "https://kochhar.com/knowledge-centre/"
+        "https://kochhar.com/knowledge-centre/page/1"
     ]
     
     def parse(self, response) :
@@ -20,6 +20,7 @@ class KochharSpider(scrapy.Spider):
             url = cards.css('h5 a').xpath("@href").get()
             date = cards.css('.date::text').get()
             yield scrapy.Request(url,callback=self.parse_document, meta={'title': title,'date': date})
+             
             
     def parse_document(self, response):
         items = KochharItem()
@@ -42,3 +43,10 @@ class KochharSpider(scrapy.Spider):
         items['Content'] = scrapePdfFromUrl(url)
         
         yield items
+        
+        next_page = "https://kochhar.com/knowledge-centre/page/"+ str(KochharSpider.page_number) + "/"
+        if KochharSpider.page_number <= 27:
+            KochharSpider.page_number += 1
+            yield response.follow(next_page, callback=self.parse)
+        
+        
